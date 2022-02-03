42039413784efbe90983f3d0c9ad2c8e1d7f8502
CrTx0 <87495218+jhonnybonny@users.noreply.github.com>
2022-02-03 23:06:48 +0200

    V_1.2
    ADD TRX2

--- install.sh
+++ install.sh
@@ -3,11 +3,12 @@
 echo -e "\e[30;41m
 Installing Auto CalypsoBTS       
 \e[0m"
-sleep 4
+sleep 3
 pip3 install pyautogui >/dev/null 2>&1
 sleep 3
 sudo cp smS.py /usr/src/CalypsoBTS
 sudo cp sub.py /usr/src/CalypsoBTS
+sudo chmod 777 autocalypsobts/console.sh autocalypsobts/nitb.sh autocalypsobts/osmobts.sh autocalypsobts/transceiver.sh autocalypsobts/trx.sh autocalypsobts/trx2.sh autocalypsobts/autobts.py
 echo -e "\e[32m
 Done !
 \e[0m"
