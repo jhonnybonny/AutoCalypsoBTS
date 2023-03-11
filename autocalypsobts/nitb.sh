71c6ab4264a6693a5dbf42d0b7e5068b998f55b0
CrTx0 <87495218+jhonnybonny@users.noreply.github.com>
2023-03-11 16:46:40 +0200

    Update nitb.sh

--- autocalypsobts/nitb.sh
+++ autocalypsobts/nitb.sh
@@ -2,5 +2,5 @@
 
 echo "DB UP"
 cd /usr/src/CalypsoBTS
-sudo osmo-nitb -c openbsc.cfg -l hlr.sqlite3 -P -C --debug=DRLL:DCC:DMM:DRR:DRSL:DNM
+sudo osmo-nitb --yes-i-really-want-to-run-prehistoric-software -c openbsc.cfg -l hlr.sqlite3 -P -C --debug=DRLL:DCC:DMM:DRR:DRSL:DNM
 read e 
